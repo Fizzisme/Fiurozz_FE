05edb36986b83c9176bd3414198bd54a72a03de6
Fizzisme <nguyenletuanphi12c1.2022@gmail.com>
2026-08-27 21:48:36 +0700

    fix(category-service): remote file categoryService

--- services/categoryService.ts
+++
@@ -1,9 +0,0 @@
-
-
-
-export const categoryService = {
-    async getAllCategories(): Promise<Exam[]> {
-        const response = await apiClient.get<ApiEnvelope<Exam[]>>('/api/exams', options);
-        return response.data;
-    },
-}
